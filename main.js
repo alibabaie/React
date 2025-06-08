09c3ecb68e17d8743c8b69d9d82b5304a892f19f
ali <mechanical.engineer.babaie@gmail.com>
2025-06-08 09:19:57 +0430

    section1 session6

--- main.js
+++ main.js
@@ -1,68 +1,12 @@
-// Var & Let & Const
+import mydefault , { testString , testFunc , TestClass } from './module1.js'
 
-if (true) {
-    
-    var x = 2 
-    
-    let y = 3 
-    
-    const z = 4 
-    
-    x=5
-    y=5
-    // z=5
+console.log(testString);
 
-    
-// console.log("x:"+x);
-// console.log("y:"+y);
-// console.log("z:"+z);
+console.log(testFunc (5));
 
-}
+let obj = new TestClass ("Ali");
+obj.handleLog();
 
-
-// console.log("x:"+x);
-// console.log("y:"+y);
-// console.log("z:"+z);
-
-// Arrow Function
-
-function test (x , y) {
-
-    console.log(x+y);
-
-}
-
-const test2 = (x , y) => {
-
-    console.log(x+y);
-
-}
-
-const test3 =  y => {
-
-    console.log(y);
-
-}
-
-const test4 =  y => {return y*2}
-
-const test5 =  y =>  y*2
-    
-
-
-test(2 , 3)
-
-test2(2 , 3)
-
-test3(4)
-
-console.log(test4(5));
-
-console.log(test5(5));
-
-
-
-
-
+console.log(mydefault);
 
 
---
+++ module2.js
@@ -0,0 +1,4 @@
+export let testNumber = 111 ;
+
+import mydefault from "./module3.js"
+console.log(mydefault);
