41a0ff72f2ac686c226ec9a829475146a7b04126
ali <mechanical.engineer.babaie@gmail.com>
2025-06-10 10:11:51 +0430

    section2 session8

--- react-project/src/index.js
+++ react-project/src/index.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 
-// let elem = <h1>سلام</h1>
+class Timer extends React.Component {
+render () {
+return (
+<h2>
+it is {new Date().toLocaleTimeString()}
+</h2>
+)
+}
+}
+
+class Salam extends React.Component {
+render () {
+return (
+<h1>salam</h1>
+)
+}
+}
+
+class App extends React.Component {
+  
+render () {
+
+return (
+<div>
+<Salam/>
+<Timer/>
+</div>
+)
+}
+}
+
+// let elem = new App ()
 
 const tick = () => {
 
-const elem = (
-  <div>
-    <h1>salam</h1>
-    <h2>
-      it is {new Date().toLocaleTimeString()}
-    </h2>
-  </div>
-)
-createRoot(document.getElementById('root')).render(elem);
+createRoot(document.getElementById('root')).render(<App/>);
+
 }
 
 setInterval(
